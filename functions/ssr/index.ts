5bccd8caa1c07d1fc504a65cfc3752564d760f79
Juho Vepsäläinen <bebraw@gmail.com>
2024-01-17 16:14:13 +0200

    feat: Add a SSR demo

--- functions/ssr/index.ts
+++ functions/ssr/index.ts
@@ -1,15 +1,21 @@
-import { baseTemplate } from "../../templates/vanilla.ts";
+import { baseTemplate, islandTemplate } from "../../templates/vanilla.ts";
+import { repeat } from "../../utils.ts";
 
 export async function onRequest() {
   // Wait 100ms to simulate load
   await new Promise((r) => setTimeout(r, 100));
 
-  // TODO: Make the contents of this page match the islands one
+  // Here all islands are prerendered instantly and there's no specific additional logic related to them
+  const island = (i: number) =>
+    `<div style="height: 10em; background-color: #${(1000 + i * 10).toString(16)}; margin: 1em; padding: 1em;">
+      ${islandTemplate(i.toString())}
+    </div>`;
+
   return new Response(
     await baseTemplate({
       base: "/ssr/",
       title: "SSR test",
-      content: "SSR test",
+      content: repeat(island, 1000).join(""),
     }),
     {
       status: 200,
