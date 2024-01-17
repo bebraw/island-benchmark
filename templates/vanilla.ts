ce4cd7d11153a7c19c8e4a1af0cc0ca2c97db7c2
Juho Vepsäläinen <bebraw@gmail.com>
2024-01-17 09:57:59 +0200

    chore: Add a basic template

---
+++ templates/vanilla.ts
@@ -0,0 +1,27 @@
+function baseTemplate({
+  base,
+  title,
+  content,
+}: {
+  base: string;
+  title: string;
+  content: string;
+}) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      <base href="${base}" />
+      <title>${title}</title>
+    </head>
+    <body>
+      <main>
+        <h1>${title}</h1>
+        <div>${content}</div>
+      </main>
+    </body>
+  </html>`;
+}
+
+export { baseTemplate };
