f93ca702e454e411aa177741cd615433f77736bb
Juho Vepsäläinen <bebraw@gmail.com>
2024-01-18 13:08:40 +0200

    chore: Add Request to Node.js conversion

--- ssr-server-node.ts
+++ ssr-server-node.ts
@@ -1,17 +1,20 @@
 import http from "http";
 import process from "process";
-
-// TODO
-// import { onRequest } from "./functions/ssr/index.ts";
+import { onRequest } from "./functions/ssr/index.ts";
 
 const port = process.env["PORT"] || 8080;
 
 http
-  .createServer((_, resp) => {
-    resp.writeHead(200, {
-      "content-type": "text/plain",
-    });
-    resp.end("Hello world");
+  .createServer(async (_, response) => {
+    // Convert a Response object to something Node can understand
+    const res = await onRequest();
+    const text = await res.text();
+
+    response.writeHead(
+      res.status,
+      JSON.stringify(Object.fromEntries(res.headers))
+    );
+    response.end(text);
   })
   .listen(port);
 console.log(`HTTP server running. Access it at: http://localhost:${port}/`);
